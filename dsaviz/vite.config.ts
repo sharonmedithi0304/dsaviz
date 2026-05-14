3223cf9170fea361239109d081aed7a03de3be5a
sharonmedithi0304 <sharonmedithi23@gmail.com>
2026-05-14 19:22:06 +0530

    fix: base path for GitHub Pages

--- dsaviz/vite.config.ts
+++ dsaviz/vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/dsaviz/',
   plugins: [react()],
+  base: '/dsaviz/',  // 👈 add this
 })
